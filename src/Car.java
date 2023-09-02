6c406efa037de16214b70ab0d7f053a8cf9dd054
Magay Denis <qwertydenis009@gmail.com>
2023-09-03 00:47:36 +0300

    HomeWork15

---
+++ src/Car.java
@@ -0,0 +1,9 @@
+public class Car extends MotorTransport{
+    public Car(String modelName) {
+        super(modelName, 4);
+    }
+
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+}
